fde7b33089a3071083e4447e790e81292dbd407c
Vitzthum Manuel (DCCS) <mvitzthum@dccsintra.net>
2020-06-29 11:43:58 +0200

    useAsync hook

---
+++ src/useAsync.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+const useAsync = (asyncFn, dependencies) => {
+  const [data, setData] = useState(null);
+  const [state, setState] = useState("idle");
+
+  useEffect(() => {
+    setState("loading");
+    setData(null);
+
+    asyncFn()
+      .then(data => {
+        setState("idle");
+        setData(data);
+      })
+      .catch(err => {
+        setState("error");
+      });
+    // eslint-disable-next-line
+  }, dependencies);
+};
