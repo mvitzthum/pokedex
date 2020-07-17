33a8c027e09b51f2ba8785e2850de2b105691061
Vitzthum Manuel (DCCS) <mvitzthum@dccsintra.net>
2020-07-17 10:06:49 +0200

    PokemonDetails functional+useEffect

--- src/columns/PokemonDetails/index.js
+++ src/columns/PokemonDetails/index.js
@@ -1,95 +1,77 @@
 // Renders the profile and games of a single pokemon
-import React from "react";
+import React, { useEffect } from "react";
 import { Spinner } from "@nice-boys/components";
 import PokemonProfile from "../../components/PokemonProfile";
 import PokemonGamesSection from "../../components/PokemonGamesSection";
 import Column from "../../components/Column";
 import { fetchPokemonGames, fetchPokemonByName } from "../../api/pokeapi";
 
-class PokemonGames extends React.Component {
-  state = {
-    games: null
-  };
+const PokemonGames = props => {
+  const [games, setGames] = React.useState(null);
+  const [state, setState] = React.useState("idle");
 
-  componentDidMount() {
-    this.fetchGames();
-  }
+  useEffect(() => {
+    if (!props.pokemon) return;
 
-  componentDidUpdate(prevProps) {
-    if (
-      (!prevProps.pokemon && this.props.pokemon) ||
-      prevProps.pokemon.name !== this.props.pokemon.name
-    ) {
-      this.fetchGames();
-    }
-  }
+    setGames(null);
 
-  fetchGames() {
-    this.setState({
-      games: null
-    });
+    setState("loading");
 
-    if (!this.props.pokemon) return;
-    fetchPokemonGames(
-      this.props.pokemon.game_indices.map(game => game.version.name)
-    ).then(games => {
-      this.setState({
-        games
+    fetchPokemonGames(props.pokemon.game_indices.map(game => game.version.name))
+      .then(games => {
+        setGames(games);
+      })
+      .catch(() => {
+        setState("error");
+      })
+      .finally(() => {
+        setState("idle");
       });
-    });
-  }
+  }, [props.pokemon]);
 
-  render() {
-    return !this.state.games ? (
-      <Spinner />
-    ) : (
-      <PokemonGamesSection games={this.state.games} />
-    );
-  }
-}
+  return (
+    <>
+      {state === "loading" && <Spinner />}
+      {state === "error" && <div>Something went wrong.</div>}
+      {state === "idle" && games && <PokemonGamesSection games={games} />}
+    </>
+  );
+};
 
-class Pokemon extends React.Component {
-  state = {
-    pokemon: null
-  };
+const Pokemon = props => {
+  const [pokemon, setPokemon] = React.useState(null);
+  const [state, setState] = React.useState("idle");
 
-  componentDidMount() {
-    this.fetchPokemon();
-  }
+  useEffect(() => {
+    if (!props.name) return;
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.name !== this.props.name) {
-      this.fetchPokemon();
-    }
-  }
+    setPokemon(null);
 
-  fetchPokemon() {
-    this.setState({
-      pokemon: null
-    });
-
-    if (!this.props.name) return;
-    fetchPokemonByName(this.props.name).then(pokemon => {
-      this.setState({
-        pokemon
+    setState("loading");
+    fetchPokemonByName(props.name)
+      .then(pokemon => {
+        setPokemon(pokemon);
+      })
+      .catch(() => {
+        setState("error");
+      })
+      .finally(() => {
+        setState("idle");
       });
-    });
-  }
+  }, [props.name]);
 
-  render() {
-    return (
-      <Column width={1} p={4}>
-        {!this.props.name ? null : !this.state.pokemon ? (
-          <Spinner />
-        ) : (
-          <>
-            <PokemonProfile pokemon={this.state.pokemon} />
-            <PokemonGames pokemon={this.state.pokemon} />
-          </>
-        )}
-      </Column>
-    );
-  }
-}
+  return (
+    <Column width={1} p={4}>
+      {state === "loading" && <Spinner />}
+      {state === "error" && <div>Something went wrong.</div>}
+      {state === "idle" && pokemon && (
+        <>
+          <PokemonProfile pokemon={pokemon} />
+          <PokemonGames pokemon={pokemon} />
+        </>
+      )}
+    </Column>
+  );
+};
 
 export default Pokemon;
